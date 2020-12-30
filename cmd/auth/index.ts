76ff15af4705784d9634db311ad37465d62be013
Ivan Sadykov <grandpajok@gmail.com>
2020-12-30 20:17:32 +0600

    fix: update ormconfig

--- cmd/auth/index.ts
+++ cmd/auth/index.ts
@@ -1,7 +1,9 @@
 import app from '../../packages/auth/app';
 import dotenv from 'dotenv';
+import { join } from 'path';
 
-dotenv.config();
+dotenv.config({ path: join(__dirname, '../../.env') });
+
 const port = process.env.PORT || 3001;
 async function boot() {
   const addr = await app.listen(port);
--- cmd/inventory/index.ts
+++ cmd/inventory/index.ts
@@ -1,7 +1,9 @@
 import app from '../../packages/inventory/app';
 import dotenv from 'dotenv';
+import { join } from 'path';
 
-dotenv.config();
+dotenv.config({ path: join(__dirname, '../../.env') });
+
 const port = process.env.PORT || 3000;
 async function boot() {
   const addr = await app.listen(port);
